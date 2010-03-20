82ab4472a7c9111572f6f483efd186d569ad75b0
Arian <arian@aryweb.nl>
2010-03-20 14:52:52 +0100

    Update Overlay to version 0.8

--- Source/Overlay.js
+++ Source/Overlay.js
@@ -1,6 +1,6 @@
 /*
 ---
-description:     Overlay
+description: Overlay
 
 authors:
   - David Walsh (http://davidwalsh.name)
@@ -9,17 +9,18 @@ license:
   - MIT-style license
 
 requires:
-  core/1.2.1:   '*'
+  core/1.2.1: '*'
 
 provides:
   - Overlay
 ...
 */
+
 var Overlay = new Class({
 	
-	Implements: [Options,Events],
+	Implements: [Options, Events],
 	
-	options:  {
+	options: {
 		id: 'overlay',
 		color: '#000',
 		duration: 500,
@@ -33,48 +34,72 @@ var Overlay = new Class({
 		*/
 	},
 	
-	initialize: function(container,options) {
+	initialize: function(container, options){
 		this.setOptions(options);
 		this.container = document.id(container);
-		this.overlay = new Element('div',{
+		if (Browser.Engine.trident && Browser.Engine.version <= 6) this.ie6 = true;
+		this.overlay = new Element('div', {
 			id: this.options.id,
 			opacity: 0,
 			styles: {
-				position: 'absolute',
+				position: (this.ie6) ? 'absolute' : 'fixed',
 				background: this.options.color,
 				left: 0,
 				top: 0,
 				'z-index': this.options.zIndex
 			},
 			events: {
-				click: function() {
+				click: function(){
 					this.fireEvent('click');
 				}.bind(this)
 			}
 		}).inject(this.container);
-		this.tween = new Fx.Tween(this.overlay,{ 
+		this.tween = new Fx.Tween(this.overlay, { 
 			duration: this.options.duration,
 			link: 'cancel',
 			property: 'opacity',
-			onStart: function() {
+			onStart: function(){
 				this.overlay.setStyles({
 					width: '100%',
 					height: this.container.getScrollSize().y
 				});
 			}.bind(this),
-			onComplete: function() {
+			onComplete: function(){
 				this.fireEvent(this.overlay.get('opacity') == this.options.opacity ? 'show' : 'hide');
 			}.bind(this)
 		});
+		window.addEvents({
+			'resize': function(){
+				this.resize();
+			}.bind(this),
+			'scroll': function(){
+				this.scroll();
+			}.bind(this)
+		});
 	},
-	open: function() {
+	
+	open: function(){
 		this.fireEvent('open');
 		this.tween.start(this.options.opacity);
 		return this;
 	},
-	close: function() {
+	
+	close: function(){
 		this.fireEvent('close');
 		this.tween.start(0);
 		return this;
+	},
+	
+	resize: function(){
+		this.fireEvent('resize');
+		this.overlay.setStyle('height', this.container.getScrollSize().y);
+		return this;
+	},
+	
+	scroll: function(){
+		this.fireEvent('scroll');
+		if (this.ie6) this.overlay.setStyle('left', window.getScroll().x);
+		return this;
 	}
+	
 });
