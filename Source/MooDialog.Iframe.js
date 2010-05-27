28f6df8978b8b57928f6f6786fc9b6603aecd340
Arian <arian@aryweb.nl>
2010-05-27 13:52:59 +0200

    Remove the useScrollBarIFrarme option because it should behave the same as useScrollBar that will beinherited by MooDialog

--- Source/MooDialog.Iframe.js
+++ Source/MooDialog.Iframe.js
@@ -20,29 +20,17 @@ MooDialog.Iframe = new Class({
 	
 	Extends: MooDialog,	
 
-	//fix
-	options: {
-		useScrollBarIFrame: true
-	},
-	
 	initialize: function(url,options){
 		this.parent(options);
+		options = this.options, optionsSize = options.size;
 		
-		//fix
-		//http://www.htmlcodetutorial.com/frames/_IFRAME_SCROLLING.html
-		var scrollingIFrame = "auto";
-		if (!this.options.useScrollBarIFrame) {
-			scrollingIFrame = "no"; 
-		}
-
-
 		this.setContent(
 			new Element('iframe',{
 				src: url,
 				frameborder: 0,
-				scrolling : scrollingIFrame, //fix
-				width: this.options.size.width,
-				height: this.options.size.height
+				scrolling: options.useScrollBar ? 'auto' : 'no',
+				width: optionsSize.width,
+				height: optionsSize.height
 			})
 		).open();
     }
