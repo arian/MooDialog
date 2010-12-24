3dc63fd49821de1cd8e7c35bd3f2cd14adbf1a98
Jules Bernable <jules.bernable@laposte.net>
2010-12-24 17:45:59 +0100

    added autosizing - autopositionning abilities

--- Source/MooDialog.js
+++ Source/MooDialog.js
@@ -15,14 +15,19 @@ var MooDialog = new Class({
 	Implements: [Options, Events],
 
 	options: {
-		'class': 'MooDialog',
-		title: null,
-		scroll: true, // IE
-		forceScroll: false,
-		useEscKey: true,
+		'class':       'MooDialog',
+		title:         null,
+		scroll:        true, // IE
+		forceScroll:   false,
+		useEscKey:     true,
 		destroyOnHide: true,
-		autoOpen: true,
-		closeButton: true,
+		autoOpen:      true,
+		closeButton:   true,
+    // Autosize options
+    autosize:      true,
+		offset:        { x: 0, y: 0 },
+    scale:         'max', // min, max or a float between 0 and 1
+    // Events
 		onInitialize: function(){
 			this.wrapper.setStyle('display', 'none');
 		},
@@ -46,29 +51,63 @@ var MooDialog = new Class({
 		this.setOptions(options);
 		this.options.inject = this.options.inject || document.body;
 		options = this.options;
+    
+    if(this.options.autosize)
+    {
+      this.size = {};
+      // Autosize stuff
+      if(this.options.scale == 'max' || this.options.scale > 0.85)
+      {
+        this.size.x = window.getSize().x * 0.85;
+        this.size.y = window.getSize().y * 0.85;
+      }
+      else if(0 < this.options.scale  && this.options.scale <= 0.85)
+      {
+        this.size.x = window.getSize().x * this.options.scale;
+        this.size.y = window.getSize().y * this.options.scale;
+      }
+      else if(this.options.scale == 'min' || this.options.scale == 0)
+      {
+        this.size.x = 'auto';
+        this.size.y = 'auto';
+      }
+    }
 
-		var wrapper = this.wrapper = new Element('div.' + options['class'].replace(' ', '.')).inject(options.inject);
-		this.content = new Element('div.content').inject(wrapper);
+		this.wrapper = new Element(
+      'div.' + options['class'].replace(' ', '.')
+    );
+		this.content = new Element('div.content').inject(this.wrapper);
+
+    if(this.options.autosize)
+    {
+      this.content.setStyles({'width': this.size.x, 'height': this.size.y});
+      this.wrapper.setStyles({
+        'position': 'absolute', 'overflow': 'auto',
+        'top': 0, 'left': 0, 'margin': 0,
+        'width': this.size.x, 'height': this.size.y
+      });
+    }
 
 		if (options.title){
-			this.title = new Element('div.title').set('text', options.title).inject(wrapper);
-			wrapper.addClass('MooDialogTitle');
+			this.title = new Element('div.title').set('text', options.title).inject(this.wrapper);
+			this.wrapper.addClass('MooDialogTitle');
 		}
 
 		if (options.closeButton){
 			this.closeButton = new Element('a.close', {
 				events: {click: this.close.bind(this)}
-			}).inject(wrapper);
+			}).inject(this.wrapper);
 		}
 
+    this.wrapper.inject(options.inject);
 
 		/*<ie6>*/// IE 6 scroll
 		if ((options.scroll && Browser.ie6) || options.forceScroll){
-			wrapper.setStyle('position', 'absolute');
-			var position = wrapper.getPosition(options.inject);
+			this.wrapper.setStyle('position', 'absolute');
+			var position = this.wrapper.getPosition(options.inject);
 			window.addEvent('scroll', function(){
 				var scroll = document.getScroll();
-				wrapper.setPosition({
+				this.wrapper.setPosition({
 					x: position.x + scroll.x,
 					y: position.y + scroll.y
 				});
@@ -87,7 +126,7 @@ var MooDialog = new Class({
 			if (options.destroyOnHide) this.destroy();
 		}.bind(this));
 
-		this.fireEvent('initialize', wrapper);
+		this.fireEvent('initialize', this.wrapper);
 	},
 
 	setContent: function(){
@@ -99,6 +138,8 @@ var MooDialog = new Class({
 		var type = typeOf(content);
 		if (['string', 'number'].contains(type)) this.content.set('text', content);
 		else this.content.adopt(content);
+
+    this.computePosition();
 
 		this.fireEvent('contentChange', this.content);
 
@@ -123,7 +164,46 @@ var MooDialog = new Class({
 
 	toElement: function(){
 		return this.wrapper;
-	}
+	},
+
+  computePosition: function()
+	{
+	  var docSize = document.id(document.body).getSize();
+	  //console.log(this.wrapper.getStyle('width'));
+		if(this.options.scale == 'min')
+		{
+		  this.setPosition(
+		    (docSize.x - this.wrapper.getSize().x)/2,
+		    (docSize.y - this.wrapper.getSize().y)/2,
+		    true
+		  );
+		}
+		else
+		{
+		  this.setPosition(
+		    (docSize.x - this.size.x)/2,
+		    (docSize.y - this.size.y)/2,
+		    true
+		  );
+		}
+		//console.log(this.options.size);
+	},
+	
+	setPosition: function(x,y,relative){
+		x = x + this.options.offset.x;
+		y = y + this.options.offset.y;
+		x = x < 20 ? 20 : x;
+		y = y < 20 ? 20 : y;
+		if(relative){
+			var scroll = document.id(document.body).getScroll();
+			x = x + scroll.x;
+			y = y + scroll.y;
+		}
+		this.wrapper.setStyles({
+      left: x, top: y
+		});
+		return this;
+	},
 
 });
 
